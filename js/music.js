a21091e34dfad5904ead532869fc895e288e6dc6
dement6d <93228501+dement6d@users.noreply.github.com>
2023-03-26 04:39:21 +0200

    music

--- js/music.js
+++ js/music.js
@@ -2,6 +2,14 @@ const song = new Audio('/audio/landfill-network_down.mp3');
 song.volume = 0.5;
 song.loop = true;
 
-song.oncanplay = () => {
-  song.play();
-};
+song.play().catch(() => {
+  document.addEventListener('keypress', () => {
+    song.play()
+  })
+  document.addEventListener('mouseover', () => {
+    song.play()
+  })
+  document.addEventListener('click', () => {
+    song.play()
+  })
+})
