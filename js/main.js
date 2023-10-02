6b764ff9885cd5377f66ff03bc9e3a3641015475
dement6d <93228501+dement6d@users.noreply.github.com>
2023-10-02 02:12:31 +0200

    no javascript compatible

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-$('header').load('html/header.html');
+// Header is to be loaded without Javascript
+// $('header').load('html/header.html');
 $('footer').load('html/footer.html');
 $('IgnsServer').load('html/igns.html');
 $('LatestRelease').load('html/latest_release.html');
 $('PreviousRelease').load('html/previous_release.html');
-$('Socials').load('html/socials.html');
+// Socials is to be loaded without Javascript
+// $('Socials').load('html/socials.html');
