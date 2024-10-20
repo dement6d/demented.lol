f68d28ac79354f2c3208f7a76de983cab34ad846
dement6d <fdsigjs@balls.com>
2024-10-20 03:30:55 +0200

    ah

---
+++ js/2fa.js
@@ -0,0 +1,59 @@
+var epoch_server = new Date().getTime(); // supposed to be sent by server
+var epoch_client = new Date().getTime();
+var epoch_diff = epoch_server - epoch_client; //IF POSITIVE, CLIENT BEHIND
+
+function getEpoch() {
+    return (new Date().getTime()) + epoch_diff;
+}
+
+function isValid(secret) {
+    return /^[a-z0-9]{16}$/g.test(secret)
+}
+
+function get2FASecretKey() {
+    return document.getElementById("secret").value.replace(/[^a-z0-9]/g, '');
+}
+
+function update2FACode(secret, epoch) {
+    var totp = new jsOTP.totp();
+    var code = totp.getOtp(secret, epoch);
+    var remaining = 30 - (Math.round(getEpoch() / 1000) % 30)
+
+    var result = document.getElementById("code")
+    var info = document.getElementById("info")
+
+    if (result.innerText != code) result.innerText = code;
+    result.style.color = (remaining <= 5 ? "#b00" : "inherit");
+    info.innerText = "New code in " + remaining + " second" + (remaining > 1 ? "s" : "") + ".";
+}
+
+function copyCode() {
+    var text = document.createElement('textarea');
+    text.value = document.getElementById("code").innerText;
+    document.body.appendChild(text)
+    text.select()
+    document.execCommand('copy');
+    document.body.removeChild(text);
+}
+
+function check() {
+    var secret = get2FASecretKey()
+    var div_ids = ["code", "info", "copy"]
+    if (isValid(secret)) {
+        update2FACode(secret, getEpoch());
+        div_ids.forEach(id => document.getElementById(id).style.display = "inherit");
+    } else if (secret == "") {
+        div_ids.forEach(id => document.getElementById(id).style.display = "none");
+    } else {
+        div_ids.forEach(id => document.getElementById(id).style.display = "none");
+        document.getElementById("info").style.display = "inherit"
+        document.getElementById("info").innerText = "Invalid secret key. It must be 16 characters long, only containing a-z and 0-9."
+    }
+}
+
+setTimeout(function () {
+    setInterval(function () {
+        var secret = get2FASecretKey()
+        if (isValid(secret)) update2FACode(secret, getEpoch());
+    }, 1000);
+}, 1000 - new Date().getMilliseconds())
